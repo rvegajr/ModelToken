e1e7170cbe6c81c01d5eec5da5034a870fa05305
Ricardo Vega <Ricardo.Vega@pxd.com>
2022-02-06 07:58:12 -0600

    Added payable for Dev and donation wallets

--- contracts/ModelToken.sol
+++ contracts/ModelToken.sol
@@ -2,18 +2,21 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 /*
 Requirements
 Address blocking /removal, array (external)
 Weekly Limit value of trade (external?)
 Swap at 1:1 with another coin (external)
-Dev Wallet, (external)
-donation wallet.(external)
+Dev Wallet, (external) - ERC721 payable - https://medium.com/codex/how-to-use-openzeppelins-paymentsplitter-8ba8de09dbf
+donation wallet.(external) - ERC721 payable - https://medium.com/codex/how-to-use-openzeppelins-paymentsplitter-8ba8de09dbf
 Able to freeze trade - PausableUpgradeable._pause()
 Max value of bnb allowed for trade. (External)
 Section for allowing minting (external) - ModelToken.mint()
@@ -21,8 +24,8 @@ Renting (external)
 */
 contract ModelToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, ERC1155SupplyUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
-
+    //constructor() initializer {}
+    constructor(address[] memory _payees, uint256[] memory _shares) ERC721("Token", "TT") PaymentSplitter(_payees, _shares) payable {}
     function initialize() initializer public {
         __ERC1155_init("");
         __Ownable_init();
